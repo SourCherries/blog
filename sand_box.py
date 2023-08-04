b58f7a19587bda626b2844e8196bebfb0fbc1e40
SourCherries <carl.michael.gaspar@icloud.com>
2023-08-04 11:27:36 +0400

    created loop-based python-only function with matching output to pandas cov(). pandas cov() does indeed do pairwise deletion. however it seems to always normalize by (N-1) even when ddof is set to 0. so need to use unbiased estimator in my examples whenever I either (1) use my loop function and/or (2) compare with pandas cov().

--- sand_box.py
+++ sand_box.py
@@ -4,6 +4,126 @@ from time import perf_counter
 import pandas as pd
 
 
+# Missing value
+M = np.nan
+
+
+# -------------------------------------------------------------------
+# Helper function
+import math
+
+X = np.array([[-3, 3, 0],
+              [ M, 3, 0],
+              [ 0, 0, 0],
+              [ 0, M, 0],
+              [ 3,-3, 0],
+              [ 3,-3, M]])
+# np.cov(X[range(0, 5, 2), ], rowvar=False, bias=True)
+# x = X[:,0]
+# y = X[:,1]
+# p = [a*b for a, b in zip(x,y)]
+# c = sum([v for v in p if not math.isnan(v)])
+
+def cov_loop_unbiased_upper(X):
+    number_variables = X.shape[1]
+    C = np.zeros((number_variables, number_variables))
+    for v1 in range(number_variables):
+        for v2 in range(v1+1, number_variables):
+            x = X[:,v1]
+            y = X[:,v2]
+
+            # remove observations where missing for either x or y
+            products = [a*b for a, b in zip(x,y)]
+            nx = [xi for xi, pi in zip(x, products) if not math.isnan(pi)]
+            ny = [yi for yi, pi in zip(y, products) if not math.isnan(pi)]
+
+            # mean-centered variables
+            cx = [x-sum(nx)/len(nx) for x in nx]
+            cy = [y-sum(ny)/len(ny) for y in ny]
+
+            # main result
+            products = [a*b for a, b in zip(cx,cy)]
+            C[v1, v2] = sum(products) / (len(products)-1)
+    return(C)
+
+def cov_loop_unbiased(X):
+    number_variables = X.shape[1]
+    C = np.zeros((number_variables, number_variables))
+    for v1 in range(number_variables):
+        for v2 in range(number_variables):
+            x = X[:,v1]
+            y = X[:,v2]
+
+            # remove observations where missing for either x or y
+            products = [a*b for a, b in zip(x,y)]
+            nx = [xi for xi, pi in zip(x, products) if not math.isnan(pi)]
+            ny = [yi for yi, pi in zip(y, products) if not math.isnan(pi)]
+
+            # mean-centered variables
+            cx = [x-sum(nx)/len(nx) for x in nx]
+            cy = [y-sum(ny)/len(ny) for y in ny]
+
+            # main result
+            products = [a*b for a, b in zip(cx,cy)]
+            C[v1, v2] = sum(products) / (len(products)-1)
+    return(C)
+
+C_me = pd.DataFrame(cov_loop_unbiased(X))
+C_pd = pd.DataFrame(X).cov(ddof=1)
+
+# Pandas cov() appears to be doing something different than simply
+#   excluding samples on a pairwise basis.
+#
+#   Perhaps it is doing listwise deletion?
+
+
+v1 = np.array([-3, M, 0, 0,  3,  3])
+v2 = np.array([ 3, 3, 0, M, -3, -3])
+p = v1 * v2
+p = p[np.isnan(p)==False]
+c_pair = p.mean()
+assert c_pair == C_me.iat[0,1]
+if (c_pair != C_pd.iat[0,1]):
+    print("pandas.DataFrame.cov() does not exclude samples pairwise.")
+
+X_listwise_delete = X[range(0, 5, 2), ]
+C_listwise = np.cov(X_listwise_delete, rowvar=False, bias=True)
+if (C_listwise[0,1] != C_pd.iat[0,1]):
+    print("pandas.DataFrame.cov() does not use listwise deletion.")
+
+pd.DataFrame(X).cov(ddof=1, min_periods=1, numeric_only=False).iat[0, 1]
+# -8.25
+
+pd.DataFrame(X).cov(ddof=0, min_periods=1, numeric_only=False).iat[0, 1]
+# -8.25
+
+np.cov(X_listwise_delete, rowvar=False)[0, 1]
+# -9
+
+np.cov(X_listwise_delete, rowvar=False, bias=True)[0, 1]
+# -6
+
+X_01 = X[:, 0:2]
+keep = np.isnan(X_01.prod(1)) == False
+X_pairwise_delete = X_01[keep, :]
+
+np.cov(X_pairwise_delete, rowvar=False)[0, 1]
+# -8.25
+
+np.cov(X_pairwise_delete, rowvar=False, bias=True)[0, 1]
+# -6.1875
+
+print("pandas.DataFrame.cov() same as pairwise numpy.cov(bias=False) or normalized by (N-1).")
+
+print("pandas.DataFrame.cov(ddof=0) does not seem to work. Forces ddof=1 and normalized by (N-1).")
+
+# Let's further explore
+#   np.cov(X_pairwise_delete, rowvar=False)[0, 1]
+
+
+# -------------------------------------------------------------------
+# Four-sample examples
+
 # Relation between matrix multiplication and covariance
 X = np.array([[-1,  1, -1],
               [-1,  1,  1],
@@ -32,8 +152,7 @@ toc = perf_counter()
 print(f"Looping through COV took {toc-tic:0.4f} seconds.")
 
 
-# Missing value problem
-M = np.nan
+# Missing value problems
 X = np.array([[-1, +1, -1],
               [-1, +1, +1],
               [+1, -1, -1],
@@ -47,6 +166,9 @@ X = np.array([[-1, +1, -1],
               [+1, -1, -1],
               [+1, -1, +1]])
 
+
+# -------------------------------------------------------------------
+# Three-sample examples
 
 X = np.array([[-3, 3, 0],
               [ 0, 0, 0],
