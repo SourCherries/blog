605a37bd72a3f127ec2191427461373f97895314
SourCherries <carl.michael.gaspar@icloud.com>
2023-08-07 13:34:24 +0400

    figured out tweak to optimize vectorized code by using float64 instead of int64 (otherwise, change order within np.dot. also implemented missing data versions of loop and vectorized functions. data are great. final thing is to ensure optimality of loop code

--- sand_box.py
+++ sand_box.py
@@ -3,7 +3,7 @@ import numpy.ma as ma
 import pandas as pd
 import math
 from time import perf_counter
-# import timeit
+import timeit
 
 
 # -------------------------------------------------------------------
@@ -299,8 +299,8 @@ def pa_loop(X):
     return(percent_agreement)
 
 # for-loop (large sample)
-number_samples_large, number_samples_large = 1000000, 300
-X = np.random.choice([0, 1], size=(number_samples_large, number_samples_large), replace=True)
+number_samples_large, number_variables_large = 10000, 200
+X = np.random.choice([0, 1], size=(number_samples_large, number_variables_large), replace=True)
 tic = perf_counter()
 percent_agreement = pa_loop(X)
 toc = perf_counter()
@@ -331,8 +331,40 @@ def pa_mat(X):
     A = S / number_samples              # percentage agreements (k x k)   
     return(A)
 
+def pa_mat_(X):
+    """Much slower. Making copy takes more time than transpose."""
+    number_samples, number_variables = X.shape
+    XT = np.copy(X.transpose())
+    yesYes = np.dot(XT, X)              # counts of yes-yes (k x k)
+    F = np.abs(X-1)                     # [0,1] -> [1,0]
+    FT = np.abs(XT-1)                   # [0,1] -> [1,0]
+    noNo = np.dot(FT, F)                # counts of no-no   (k x k)
+    S = yesYes + noNo                   # counts of agreements (k x k)
+    A = S / number_samples              # percentage agreements (k x k)   
+    return(A)
 
-X = np.random.choice([0, 1], size=(number_samples_large, number_samples_large), replace=True)
+def pa_mat__(X):
+    """In-place Numpy operation. Indeed better than pa_mat()"""
+    number_samples, number_variables = X.shape
+    yesYes = np.dot(X.transpose(), X)   # counts of yes-yes (k x k)
+    np.abs(X-1, out=X)                  # [0,1] -> [1,0]
+    noNo = np.dot(X.transpose(), X)     # counts of no-no   (k x k)
+    S = yesYes + noNo                   # counts of agreements (k x k)
+    A = S / number_samples              # percentage agreements (k x k)   
+    return(A)
+
+# mat mul (timed)
+def pa_mat_rev(X):
+    number_samples, number_variables = X.shape
+    yesYes = np.dot(X, X.transpose())   # counts of yes-yes (k x k)
+    F = np.abs(X-1)                     # [0,1] -> [1,0]
+    noNo = np.dot(F, F.transpose())     # counts of no-no   (k x k)
+    S = yesYes + noNo                   # counts of agreements (k x k)
+    A = S / number_samples              # percentage agreements (k x k)   
+    return(A)
+
+X = np.random.choice([0, 1], size=(number_samples_large, number_variables_large), replace=True)
+XT = X.transpose()
 tic = perf_counter()
 percent_agreement = pa_mat(X)
 toc = perf_counter()
@@ -341,10 +373,314 @@ print(f"Computing all pairwise percentage agreements took {seconds_mat:0.4f} sec
 
 print(f"Loops takes {seconds_loop / seconds_mat:0.4f} times longer.")
 
+# number_samples_large = 1000
+# number_variables_large = [10, 50, 100, 500, 1000]
+# seconds = np.zeros((len(number_variables_large), 4))
+# for i, nvl in enumerate(number_variables_large):
+#     print(str(i) + "\n")
+#     # X = np.random.choice([0, 1], size=(number_samples_large, nvl), replace=True)
+#     X = np.random.choice([0., 1.], size=(number_samples_large, nvl), replace=True)
+#     tic = perf_counter(); percent_agreement = pa_loop(X); toc = perf_counter()
+#     seconds_loop = toc-tic
+
+#     tic = perf_counter(); percent_agreement = pa_mat(X); toc = perf_counter()
+#     seconds_mat = toc-tic
+
+#     tic = perf_counter(); percent_agreement = pa_mat__(X); toc = perf_counter()
+#     seconds_mat_ = toc-tic
+
+#     tic = perf_counter(); percent_agreement = pa_mat_rev(XT); toc = perf_counter()
+#     seconds_mat_rev = toc - tic
+
+#     seconds[i, 0] = seconds_loop
+#     seconds[i, 1] = seconds_mat
+#     seconds[i, 2] = seconds_mat_
+#     seconds[i, 3] = seconds_mat_rev
 
 # kappa (no need to explain)
+
+# import matplotlib.pyplot as plt
+# seconds = np.array([[4.52540000e-04, 1.64543002e-04],
+#        [5.14091700e-03, 5.63542700e-03],
+#        [1.85514320e-02, 1.90126080e-02],
+#        [4.45230638e-01, 5.00560685e-01],
+#        [1.80888819e+00, 2.05097258e+00],
+#        [6.66040610e+01, 9.29598458e+01]])
+
+# plt.loglog(number_variables_large, seconds); plt.show()
+
+# S = pd.DataFrame(seconds, columns=["loop", "vectorized"], index=number_variables_large)
+# S.plot(loglog=True)
+# plt.show()
+
+
+
+
+
+X = np.random.choice([0, 1], size=(number_samples_large, 1000), replace=True)
+
+tic = perf_counter()
+np.dot(X.transpose(), X)
+toc = perf_counter()
+print(f"{toc-tic:0.4f} seconds.")
+
+tic = perf_counter()
+np.dot(X, X.transpose())
+toc = perf_counter()
+print(f"{toc-tic:0.4f} seconds.")
+
+
+# np.dot
+tran_first = """
+X = np.random.choice([0, 1], size=(1000, 1000), replace=True);
+np.dot(X.transpose(), X)
+"""
+tran_second = """
+X = np.random.choice([0, 1], size=(1000, 1000), replace=True);
+np.dot(X, X.transpose())
+"""
+tf = timeit.Timer(tran_first, setup="import numpy as np")
+print(tf.timeit(number=10)/10)
+
+ts = timeit.Timer(tran_second, setup="import numpy as np")
+print(ts.timeit(number=10)/10)
+
+# np.matmul
+tran_first = """
+X = np.random.choice([0, 1], size=(1000, 1000), replace=True);
+np.matmul(X.transpose(), X)
+"""
+tran_second = """
+X = np.random.choice([0, 1], size=(1000, 1000), replace=True);
+np.matmul(X, X.transpose())
+"""
+tf = timeit.Timer(tran_first, setup="import numpy as np")
+print(tf.timeit(number=10)/10)
+
+ts = timeit.Timer(tran_second, setup="import numpy as np")
+print(ts.timeit(number=10)/10)
+
+
+# -------------------------------------------------------------------
+# 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
+# Include in blog (order of operations during MM and data type)
+
+# Now I make data X with same number rows and cols.
+
+# np.dot(X.transpose(), X)  # about 1.4 seconds
+
+# np.dot(X, X.transpose())  # about 0.48 seconds
+
+# This is quite dramatic.
+
+# That was with X dims (1000, 1000)
+
+# Now that was with X dtype int64
+
+# If I use X dtype float64, we get the reverse
+
+# np.dot(X.transpose(), X)  # 0.0085 seconds
+
+# np.dot(X, X.transpose())  # 0.018 seconds
+
+# simulations with int64
+#   X = np.random.choice([0, 1], size=(number_samples_large, nvl), replace=True)
+#   in loop
+
+# >>> pd.DataFrame(seconds, columns=['loop','mat','mat__','mat_rev'])
+#        loop       mat     mat__   mat_rev
+# 0  0.000170  0.000219  0.000150  1.727916
+# 1  0.004799  0.004580  0.004512  1.537690
+# 2  0.018997  0.021193  0.019723  1.728106
+# 3  0.453382  0.512756  0.509368  1.610502
+# 4  1.822136  2.089260  2.087054  1.666854
+
+# simulations with float64
+#   X = np.random.choice([0., 1.], size=(number_samples_large, nvl), replace=True)
+#   in loop
+
+# >>> pd.DataFrame(seconds, columns=['loop','mat','mat__','mat_rev'])
+#        loop       mat     mat__   mat_rev
+# 0  0.000243  0.000364  0.000053  1.764093
+# 1  0.004907  0.000346  0.000161  1.574247
+# 2  0.018095  0.001281  0.000521  1.572380
+# 3  0.481681  0.009352  0.006546  1.913391
+# 4  1.988601  0.032003  0.026432  1.992859
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def pa_loop(X):
+    number_samples, number_variables = X.shape
+    percent_agreement = np.zeros((number_variables, number_variables))
+    for item_a in range(number_variables):
+        for item_b in range(item_a+1, number_variables):
+            percent_agreement[item_a, item_b] = (X[:, item_a]==X[:, item_b]).sum()
+    percent_agreement /= number_samples
+    return(percent_agreement)
+
+def pa_vect(X):
+    number_samples, _ = X.shape
+    yesYes = np.dot(X.transpose(), X)   # counts of yes-yes (k x k)
+    np.abs(X-1, out=X)                  # [0,1] -> [1,0]
+    noNo = np.dot(X.transpose(), X)     # counts of no-no   (k x k)
+    S = yesYes + noNo                   # counts of agreements (k x k)
+    A = S / number_samples              # percentage agreements (k x k)   
+    return(A)
+
+number_samples_large = 1000
+number_variables_large = [10, 50, 100, 500, 1000]
+seconds = np.zeros((len(number_variables_large), 2))
+for i, nvl in enumerate(number_variables_large):
+    print(str(i) + "\n")
+    X = np.random.choice([0., 1.], size=(number_samples_large, nvl), replace=True)
+
+    tic = perf_counter(); percent_agreement = pa_loop(X); toc = perf_counter()
+    seconds_loop = toc-tic
+
+    tic = perf_counter(); percent_agreement = pa_vect(X); toc = perf_counter()
+    seconds_vect = toc-tic
+
+    seconds[i, 0] = seconds_loop
+    seconds[i, 1] = seconds_vect
+
+df = pd.DataFrame(seconds, 
+                  columns=['loop','vectorized'],
+                  index=number_variables_large)
+
+# df.plot(loglog=True, xlabel="Number of variables", ylabel="Seconds")
+# plt.show()
+
+# speed_up = seconds[:,0] / seconds[:,1]
+# plt.semilogx(number_variables_large, speed_up)
+# plt.xlabel("Number of variables")
+# plt.ylabel("Speed advantage")
+# plt.title("Vectorized operations beats loops")
+# plt.show()
+
+
+
+# To plot in another terminal because ... strange conda inconsistency
+# seconds = np.array([[2.43103000e-04, 3.63571000e-04, 5.31710002e-05, 1.76409273e+00],
+#        [4.90698300e-03, 3.46419000e-04, 1.61126000e-04, 1.57424746e+00],
+#        [1.80947730e-02, 1.28098600e-03, 5.20920000e-04, 1.57238003e+00],
+#        [4.81681406e-01, 9.35171500e-03, 6.54643100e-03, 1.91339133e+00],
+#        [1.98860055e+00, 3.20029940e-02, 2.64323600e-02, 1.99285874e+00]])
+# df = pd.DataFrame(seconds[:,[0,2]], 
+#                   columns=['loop','vectorized_inplace'],
+#                   index=number_variables_large)
+# df.plot(loglog=True, xlabel="Number of variables", ylabel="Seconds")
+# plt.show()
+
+# speed_up = seconds[:,0] / seconds[:,2]
+# plt.semilogx(number_variables_large, speed_up)
+# plt.xlabel("Number of variables")
+# plt.ylabel("Speed advantage")
+# plt.title("Vectorized operations beats loops")
+# plt.show()
+n_samples, n_variables = 1000, 100
+responses, probs = [0., 1., M], [.4, .4, .2]
+X = np.random.choice(responses, 
+                     size=(n_samples, n_variables), 
+                     replace=True,
+                     p=probs)
+
+
+# def pa_loop_missing(X):
+#     number_samples, number_variables = X.shape
+#     percent_agreement = np.zeros((number_variables, number_variables))
+#     for item_a in range(number_variables):
+#         for item_b in range(item_a+1, number_variables):
+#             percent_agreement[item_a, item_b] = (X[:, item_a]==X[:, item_b]).sum()
+#     percent_agreement /= number_samples
+#     return(percent_agreement)
+
+# Need specific X to test (take form unit tests)
+V = np.array([[0, 0, 0, 0, 1, 1, 1, 1],
+              [0, 0, 0, 0, 1, 1, 1, 1],
+              [0, 1, 0, 1, 0, 1, 0, 1],
+              [1, 1, 1, 1, 0, 0, 0, 0]]).transpose()
+W = np.array([[0, 0, 0, M, 1, 1, 1, M],
+              [0, 0, M, 0, 1, 1, M, 1],
+              [0, M, 0, 1, 0, M, 0, 1],
+              [M, 1, 1, 1, M, 0, 0, 0]]).transpose()
+X = np.r_[V, W]
+def pa_loop_missing(X):
+    _, number_variables = X.shape
+    percent_agreement = np.zeros((number_variables, number_variables))
+    for item_a in range(number_variables):
+        for item_b in range(item_a+1, number_variables):
+                x = X[:, item_a]
+                y = X[:, item_b]
+                # remove observations where missing for either x or y
+                products = [a*b for a, b in zip(x,y)]
+                nx = [xi for xi, pi in zip(x, products) if not math.isnan(pi)]
+                ny = [yi for yi, pi in zip(y, products) if not math.isnan(pi)]
+                percent_agreement[item_a, item_b] = sum([va==vb for va, vb in zip(nx, ny)]) / len(nx)
+    return(percent_agreement)
+
+
+
+# def pa_vect(X):
+#     number_samples, _ = X.shape
+#     yesYes = np.dot(X.transpose(), X)   # counts of yes-yes (k x k)
+#     np.abs(X-1, out=X)                  # [0,1] -> [1,0]
+#     noNo = np.dot(X.transpose(), X)     # counts of no-no   (k x k)
+#     S = yesYes + noNo                   # counts of agreements (k x k)
+#     A = S / number_samples              # percentage agreements (k x k)   
+#     return(A)
+
+def pa_vect_missing(X):
+    R = ma.masked_invalid(X)  # (n x k)
+    yesYes = ma.dot(R.transpose(), R)  # counts of yes-yes (k x k)
+    F = ma.abs(R-1)  # [0,1] -> [1,0]
+    noNo = ma.dot(F.transpose(), F)  # counts of no-no   (k x k)
+    S = yesYes + noNo  # counts of agreements (k x k)
+    valid = np.ones_like(R)  # valid responses (n x k)
+    valid[ma.getmaskarray(R)] = 0
+    N = np.dot(valid.transpose(), valid)
+    A = ma.multiply(S, N**-1)
+    return(A)
+
+
+
+responses, probs = [0., 1., M], [.4, .4, .2]
+number_samples_large = 1000
+number_variables_large = [10, 50, 100, 500, 1000]
+
+seconds_missing = np.zeros((len(number_variables_large), 2))
+for i, nvl in enumerate(number_variables_large):
+    print(str(i) + "\n")
+    X = np.random.choice(responses, 
+                        size=(number_samples_large, nvl), 
+                        replace=True,
+                        p=probs)
+    tic = perf_counter(); percent_agreement = pa_loop_missing(X); toc = perf_counter()
+    seconds_loop = toc-tic
+    tic = perf_counter(); percent_agreement = pa_vect_missing(X); toc = perf_counter()
+    seconds_vect = toc-tic
+    seconds_missing[i, 0] = seconds_loop
+    seconds_missing[i, 1] = seconds_vect
+
+df_missing = pd.DataFrame(seconds_missing, 
+                          columns=['loop','vectorized'],
+                          index=number_variables_large)
+
+
 
 
 
 # End
 # -------------------------------------------------------------------
+
+
+# Problem with matplotlib in my conda env blog
+
+# df.plot(loglog=True, xlabel="Number of variables", ylabel="Seconds")
+
+# qt.qpa.plugin: Could not load the Qt platform plugin "xcb" in "" even though it was found.
+# This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem.
+
+# Available platform plugins are: eglfs, minimal, minimalegl, offscreen, vnc, webgl, xcb.
+
+# Aborted
